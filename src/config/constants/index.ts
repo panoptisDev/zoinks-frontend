e75615052595194d27a6cf3e11a7ab95c0e82350
Nabuhiro <petrukovichmark@gmail.com>
2022-01-13 16:06:16 +0900

    change router, update sdk

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,8 @@
 import { ChainId, JSBI, Percent, Token } from '@zoinks-swap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+// export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E' // mainnet
+export const ROUTER_ADDRESS = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1' // testnet
 
 // a list of tokens by chain
 type ChainTokenList = {
